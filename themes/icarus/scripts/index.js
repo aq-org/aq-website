13de3d0fd4b2bb514060d1ace4f3698b340e167b
ax-6 <ax-6@outlook.com>
2024-05-09 21:09:01 +0800

    Some changes in aq blog. Not complete.

---
+++ themes/icarus/scripts/index.js
@@ -0,0 +1,31 @@
+/* global hexo */
+const createLogger = require('hexo-log');
+
+const logger = createLogger.default();
+
+/**
+ * Print welcome message
+ */
+logger.info(`=======================================
+ ██╗ ██████╗ █████╗ ██████╗ ██╗   ██╗███████╗
+ ██║██╔════╝██╔══██╗██╔══██╗██║   ██║██╔════╝
+ ██║██║     ███████║██████╔╝██║   ██║███████╗
+ ██║██║     ██╔══██║██╔══██╗██║   ██║╚════██║
+ ██║╚██████╗██║  ██║██║  ██║╚██████╔╝███████║
+ ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝
+=============================================`);
+
+/**
+ * Check if all dependencies are installed
+ */
+require('../include/dependency')(hexo);
+
+/**
+ * Configuration file checking and migration
+ */
+require('../include/config')(hexo);
+
+/**
+ * Register Hexo extensions and remove Hexo filters that could cause OOM
+ */
+require('../include/register')(hexo);
