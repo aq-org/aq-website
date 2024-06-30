3521c7fd2e778b0dfa8a26ab77e88c9cfa95a36d
ax-6 <ax-6@outlook.com>
2024-06-30 17:46:37 +0800

    Uploaded the zh version of AQ Official Website.

--- src/pages/zh/rss.xml.ts
+++ src/pages/zh/rss.xml.ts
@@ -15,7 +15,7 @@ export const GET = async () => {
   const posts = await fetchPosts();
 
   const rss = await getRssString({
-    title: `${SITE.name}’s Blog`,
+    title: `${SITE.name}的博客`,
     description: METADATA?.description || '',
     site: import.meta.env.SITE,
 
